4efe0e404814d9071587074fb6a72fd45016801f
David-138 <131913487+David-138@users.noreply.github.com>
2026-04-15 17:54:23 +0100

    el ovni finalmente funciona

--- Assets/Scripts/UFO.cs
+++ Assets/Scripts/UFO.cs
@@ -1,27 +1,63 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.UIElements;
 
 public class UFO : MonoBehaviour
 {
     public Transform routeFather;
-    int indexChildren;
+    public bool recoger;
+    public bool volver;
     Vector3 destination;
+    [SerializeField] GameObject pitcher;
 
     private void Start()
     {
-        destination = routeFather.GetChild(indexChildren).position;
+        volver = false;
+        recoger = true;
+        destination = new Vector3(routeFather.GetChild(0).position.x,transform.position.y, routeFather.GetChild(0).position.z);
         GetComponent<NavMeshAgent>().SetDestination(destination);
     }
 
     void Update()
     {
-        if (Vector3.Distance(transform.position, destination) < 2.5f)
+        Debug.Log(routeFather.GetChild(0).gameObject.transform.position);
+        if (Vector3.Distance(transform.position, destination) < 0.5f)
         {
-            indexChildren++;
-            if (indexChildren >= routeFather.childCount)
-                indexChildren = 0;
-            destination = routeFather.GetChild(indexChildren).position;
+            if (volver)
+            {
+                volver = false;
+                StartCoroutine(Refill());
+            }
+            if (recoger)
+            {
+                recoger = false;
+                StartCoroutine(Abduction());
+            }
+        }
+    }
+    private IEnumerator Abduction()
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (routeFather.GetChild(0).position.y < 4)
+        {
+            routeFather.GetChild(0).GetComponent<Rigidbody>().AddForce(0,250,0);
+            StartCoroutine(Abduction());
+        }
+        else
+        {
+            Destroy(routeFather.GetChild(0).gameObject);
+            destination = new Vector3(pitcher.transform.position.x, transform.position.y, pitcher.transform.position.z);
+            volver = true;
             GetComponent<NavMeshAgent>().SetDestination(destination);
         }
+    }
+    private IEnumerator Refill()
+    {
+        yield return new WaitForSeconds(1f);
+        destination = new Vector3(routeFather.GetChild(0).position.x, transform.position.y, routeFather.GetChild(0).position.z);
+        GetComponent<NavMeshAgent>().SetDestination(destination);
+        recoger = true;
     }
 }
